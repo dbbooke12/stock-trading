97506e9054ecc133a63d5c4ef5dd6aceb3c48d79
Darren Booker <darrenbooker@Darrens-MacBook-Pro.local>
2019-10-19 22:29:58 -0700

    updated vuex

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
-        <h1>Stocks components</h1>
+        <app-stock v-for="stock in stocks" v-bind:key="stock.id" :stock="stock">{{ stock }}</app-stock>
     </div>
 </template>
+
+<script>
+import Stock from './Stock.vue'
+
+
+export default {
+    components: {
+        appStock: Stock
+    },
+    computed: {
+        stocks() {
+            return this.$store.getters.stocks;
+        }
+    }
+}
+</script>
---
+++ src/data/stocks.js
@@ -0,0 +1,7 @@
+export default [
+    { id: 1, name: 'BMW', price: 110 },
+    { id: 2, name: 'Apple', price: 75 },
+    { id: 3, name: 'Amazon', price: 1000 },
+    { id: 4, name: 'Youtube', price: 201 },
+    { id: 5, name: 'Zappos', price: 183 }
+];
